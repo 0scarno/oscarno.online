6278ee0a8d812230491f61b2fef0f00eea3e80e6
Oscar J <oscarjemmott@gmail.com>
2025-10-01 15:27:07 +1000

    drafts working

--- .eleventy.js
+++ .eleventy.js
@@ -30,8 +30,8 @@ export default function(eleventyConfig){
         );
     });
 
-    eleventyConfig.addPreprocessor("drafts", "*", (data, content) => {
-		if(data.draft && process.env.ELEVENTY_RUN_MODE === "build") {
+    eleventyConfig.addPreprocessor("drafts", "njk,md,liquid", (data, content) => {
+		if(data.draft) {
 			return false;
 		}
 	});
